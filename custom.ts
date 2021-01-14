e1f98255db4ec3ca89236d39a6998adc54fa88c5
vishalmakebot <77429985+vishalmakebot@users.noreply.github.com>
2021-01-14 12:08:27 +0530

    Initial files for MakeCode project

---
+++ custom.ts
@@ -0,0 +1,20 @@
+
+/**
+ * Makebot blocks
+ */
+//% weight=80 color=#0fbc11 icon=""
+namespace Makebot {
+    /**
+     * @param c is for coloum
+     * @param r is for rows 
+     * @param t is for Interval time
+     */
+    //% block="Blink coloum $c row $r Interval time $t"
+     //% c.min=0 c.max=4 r.min=0 r.max=4
+    //% c.fieldOptions.precision=1 r.fieldOptions.precision=1
+    export function Makebot (c: number, r: number, t: number): void {
+      let start= game.createSprite(c,r)
+      start.setBlink(t)
+      start.blink()  
+    }
+}
